be9d7ae6102ef4ce85634e58829c69ab5bc1123f
Bradley Cicenas <bradley.cicenas@gmail.com>
2015-12-07 14:56:35 -0500

    initial site commit

---
+++ themes/command/static/js/typing.js
@@ -0,0 +1,32 @@
+
+//The function
+function typeIt($id, $text, $delay, $cursorCount)
+{  
+  var flashSpeed = 400;  
+  
+  for (var i = 0; i < $cursorCount * 2; i+=2)
+  {
+    window.setTimeout("$('" + $id + "').html('|')",   (i)     * flashSpeed);
+    window.setTimeout("$('" + $id + "').html('_')",    (i + 1) * flashSpeed);
+  }
+  
+  
+  window.setTimeout("addLetter('" + $id + "','" + $text + "'," +$delay +" ,1); ", $cursorCount * 2 * flashSpeed);
+  
+}
+
+
+//Adds a single letter then recurses back on itself on a timer
+function addLetter($id, $text, $delay, $i)
+{  
+  var $textToSet = $text.substring(0,$i);
+  if ($textToSet != $text)
+  {    
+    $($id).html($textToSet);
+    window.setTimeout(function(){addLetter($id, $text,$delay, $i+1)},$delay);
+  }
+  else
+  {
+    $($id).html($textToSet);    
+  }  
+}
