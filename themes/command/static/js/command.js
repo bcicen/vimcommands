8e4ef61b5d0cfcc94f7a2adaf2291760d16f6a5a
Bradley Cicenas <bradley.cicenas@gmail.com>
2015-12-10 12:07:00 -0500

    rename js, add about

--- themes/command/static/js/command.js
+++ themes/command/static/js/command.js
@@ -1,20 +1,18 @@
 
-//The function
 function typeIt($id, $text, $delay, $cursorCount)
 {  
   var flashSpeed = 400;  
-  
+
   for (var i = 0; i < $cursorCount * 2; i+=2)
   {
     window.setTimeout("$('" + $id + "').html('|')",   (i)     * flashSpeed);
     window.setTimeout("$('" + $id + "').html('_')",    (i + 1) * flashSpeed);
   }
-  
-  
-  window.setTimeout("addLetter('" + $id + "','" + $text + "'," +$delay +" ,1); ", $cursorCount * 2 * flashSpeed);
-  
-}
 
+
+  window.setTimeout("addLetter('" + $id + "','" + $text + "'," +$delay +" ,1); ", $cursorCount * 2 * flashSpeed);
+
+}
 
 //Adds a single letter then recurses back on itself on a timer
 function addLetter($id, $text, $delay, $i)
